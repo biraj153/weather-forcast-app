fc14e93a8b4ab71a2119218b76eade03e82fde44
Biraj Adhikari <birajadhikari152@gmail.com>
2024-06-25 18:23:04 +0545

    Added Location Selector

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $response = Http::get('http://api.weatherapi.com/v1/current.json?key=57cbb39d84354f1aaae194212242206&q=Kathmandu&aqi=no');
+        $location = $request->input('location', 'Kathmandu'); // default to Kathmandu if no location is provided
+
+        if ($request->input('location', 'Kathmandu')) {
+            $location = 'Kathmandu';
+        }
+
+        $response = Http::get("http://api.weatherapi.com/v1/current.json?key=57cbb39d84354f1aaae194212242206&q={$location}&aqi=no");
         $weather = $response->json();
-        // dd($weather);
+
         return view('weather.index', ['weather' => $weather]);
     }
 }
--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
-    <div class="card">
-        <div class="card-header border-none text-center">
+    <div class="card mb-3 mx-5 bg-sky-600">
+        <div class="card-header border-none text-center ">
             <h3>Weather</h3>
         </div>
-        <div class="card-body container-fluid border-none px-5">
+        <div class="card-body container-fluid border-none px-5 ">
             <div class="row justify-content-between">
                 <div class="col-8 text-left">
                     <h5 class="card-title">{{ $weather['location']['name'] }}, {{ $weather['location']['country'] }}</h5>
                     <p class="card-text">Current Temperature: {{ $weather['current']['temp_c'] }}&deg;C |
                         {{ $weather['current']['temp_f'] }}&deg;F</p>
                 </div>
-                <div class="col-4 ">
+                <div class="col-4">
                     <div class="text-end">
                         <img src="{{ $weather['current']['condition']['icon'] }}">
                         {{ $weather['current']['condition']['text'] }}
@@ -20,27 +20,34 @@
             </div>
         </div>
     </div>
-    <select class="form-select" aria-label="Default select example">
-        <option selected>Select Location</option>
-        <option value="1">Baglung</option>
-        <option value="2">Bharatpur</option>
-        <option value="3">Bhaktapur</option>
-        <option value="4">Biratnagar</option>
-        <option value="5">Birendranagar</option>
-        <option value="6">Birgunj</option>
-        <option value="7">Butwal</option>
-        <option value="8">Damak</option>
-        <option value="9">Dhangadhi</option>
-        <option value="10">Ghorahi</option>
-        <option value="11">Hetauda</option>
-        <option value="12">Itahari</option>
-        <option value="13">Janakpur</option>
-        <option value="14">Kathmandu</option>
-        <option value="15">Lahan</option>
-        <option value="16">Nepalgunj</option>
-        <option value="17">Pokhara</option>
-        <option value="18">Rajbiraj</option>
-        <option value="19">Siddharthanagar</option>
-        <option value="20">Tulsipur</option>
-    </select>
+    <form method="GET" action="{{ route('weather.index') }}" class="align-center mx-5">
+        <div class="row">
+            <div class="col-10">
+                <select class="form-select" aria-label="Default select example" name="location">
+                    <option value="1" selected>Select Location</option>
+                    <option value="Baglung">Baglung</option>
+                    <option value="Bhaktapur">Bhaktapur</option>
+                    <option value="Biratnagar">Biratnagar</option>
+                    <option value="Birgunj">Birgunj</option>
+                    <option value="Butwal">Butwal</option>
+                    <option value="Dhangadhi">Dhangadhi</option>
+                    <option value="Ghorahi">Ghorahi</option>
+                    <option value="Hetauda">Hetauda</option>
+                    <option value="Itahari">Itahari</option>
+                    <option value="Janakpur">Janakpur</option>
+                    <option value="Kathmandu">Kathmandu</option>
+                    <option value="Lahan">Lahan</option>
+                    <option value="Pokhara">Pokhara</option>
+                    <option value="Rajbiraj">Rajbiraj</option>
+                    <option value="Siddharthanagar">Siddharthanagar</option>
+                    <option value="Tulsipur">Tulsipur</option>
+                </select>
+            </div>
+            <div class="col-2">
+                <button type="submit" class="btn btn-primary">Get Weather</button>
+            </div>
+        </div>
+    </form>
+
+    </form>
 </x-layout>
