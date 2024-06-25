ea1651827196d42be9ab1a5ea3ab84b82dc37c95
Biraj Adhikari <birajadhikari152@gmail.com>
2024-06-25 17:49:18 +0545

    Added Weather Controller Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeatherController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [WeatherController::class, 'index'])->name('weather.index');
